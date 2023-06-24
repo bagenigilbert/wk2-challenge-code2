032350c139d23f2b0a52fd20990190b7042c23af
gilbert <gilbert.bageni@student.moringaschool.com>
2023-06-24 18:53:11 +0300

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const animalList = document.querySelector('animalList')
+    const btn = document.createElement("button")
+    btn.innerText ="SELECT CONTESTANT"
+    animalList.append(btn)
+});
